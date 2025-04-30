8090efc322f5f5f6d131927e1ba003e10d61bd93
Pablo Orduña <pablo@labsland.com>
2025-04-30 16:40:33 -0700

    Add morse

--- simulations.cmake
+++ simulations.cmake
@@ -5,6 +5,7 @@ set(SIMULATIONS_CPP_FILES
     hybridapi/src/labsland/simulations/watertanksimulation.cpp
     hybridapi/src/rhlab/butterfly.cpp
     hybridapi/src/rhlab/matrix.cpp
+    hybridapi/src/rhlab/morse.cpp
     hybridapi/src/deusto/door.cpp
     hybridapi/src/deusto/watertankDeusto.cpp
 )
